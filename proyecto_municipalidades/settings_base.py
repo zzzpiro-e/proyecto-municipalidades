380dc192d81503ed249ff3513d6ca52836812152
Derikoso <derick9103@gmail.com>
2025-09-30 00:30:53 -0300

    Se agregaron las apps al settings_base.py

--- proyecto_municipalidades/settings_base.py
+++ proyecto_municipalidades/settings_base.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "core",
+    "direccion",
+    "departamento",
+    "territorial",
+    "encuesta",
+    "incidencia",
 ]
 
 MIDDLEWARE = [
