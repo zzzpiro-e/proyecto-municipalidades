40b8aeaa27b06b7d39d7583d232f0c8197713e86
Derikoso <derick9103@gmail.com>
2025-10-01 20:42:46 -0300

    Se cambia el metodo cascade por set_null
    para que no se elimine el departamento en caso de eliminar al usuario encargado

--- departamento/models.py
+++ departamento/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from direccion.models import Direccion
 
 class Departamento(models.Model):
-    usuario=models.OneToOneField(User, on_delete=models.CASCADE)
+    usuario=models.OneToOneField(User, on_delete=models.SET_NULL,null=True,blank=True)
     direccion=models.ForeignKey(Direccion, on_delete=models.CASCADE)
     nombre_departamento=models.CharField(max_length=200,null=False,blank=False)
     state=models.CharField(max_length=100,null=True,blank=True,default='Activo')
@@ -17,3 +17,4 @@ class Departamento(models.Model):
 
     def __str__(self):
         return self.nombre_departamento
+
