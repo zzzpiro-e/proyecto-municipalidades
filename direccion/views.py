d3929f22bcb3c262eb8e2b5d23c8c4a565ce9432
Derikoso <derick9103@gmail.com>
2025-10-01 20:38:03 -0300

    Actualiza vistas crear y guardar direccion para poder usar select de usuario

--- direccion/views.py
+++ direccion/views.py
@@ -1,3 +1,4 @@
+
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render,redirect
@@ -11,7 +12,7 @@ def main_direccion(request):
     except:
         messages.add_message(request,messages.INFO, 'Error')
         return redirect('login')
-    if profile.group_id in [1,2]:
+    if profile.group_id ==2:
         template_name = 'direccion/main_direccion.html'
         return render(request,template_name)
     else: 
@@ -25,7 +26,8 @@ def crear_direccion(request):
         return redirect('login')
     if profile.group_id ==1:
         template_name = 'direccion/crear_direccion.html'
-        return render(request,template_name)
+        usuarios=User.objects.filter(profile__group__id=2)
+        return render(request,template_name,{"usuarios":usuarios})
     else: 
         return redirect('logout')
 
@@ -39,15 +41,13 @@ def guardar_direccion(request):
     if profile.group_id==1:
         if request.method=='POST':
             nombre_direccion=request.POST.get('nombre_direccion')
-            nombre_encargado=request.POST.get('nombre_encargado')
-            correo_encargado=request.POST.get('correo_encargado')
-            if nombre_direccion=='' or nombre_encargado=='' or correo_encargado=='':
+            usuario_id=request.POST.get('usuario')
+            if nombre_direccion=='' or not usuario_id:
                 messages.add_message(request,messages.INFO, 'Debes ingresar toda la información, no pueden quedar campos vacíos')
                 return redirect('crear_direccion')
             direccion_save=Direccion(
                 nombre_direccion=nombre_direccion,
-                nombre_encargado=nombre_encargado,
-                correo_encargado=correo_encargado,
+                usuario_id=usuario_id,
                 )
             direccion_save.save()
             messages.add_message(request,messages.INFO,'Dirección creada con exito')
@@ -57,6 +57,7 @@ def guardar_direccion(request):
             return redirect('check_group_main')
     else:
         return redirect('logout')
+
 
 
 
