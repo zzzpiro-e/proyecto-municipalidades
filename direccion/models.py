3c66356bf7d8b1e8db6251b033b6bf2abc67940b
Derikoso <derick9103@gmail.com>
2025-09-30 22:12:00 -0300

    Actualizacion atributo usuario en dirección

--- direccion/models.py
+++ direccion/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Direccion(models.Model):
-    usuario=models.OneToOneField(User, on_delete=models.CASCADE)
+    usuario=models.OneToOneField(User, on_delete=models.SET_NULL,null=True,blank=True)
     nombre_direccion=models.CharField(max_length=200,null=False,blank=False)
     nombre_encargado=models.CharField(max_length=200,null=False,blank=True)
     correo_encargado=models.EmailField(max_length=254,null=False,blank=True)
@@ -17,3 +17,4 @@ class Direccion(models.Model):
 
     def __str__(self):
         return self.nombre_direccion
+
