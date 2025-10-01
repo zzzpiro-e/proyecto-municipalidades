371b623b79d3b89098c0a85b9d65931b1c106ab5
Derikoso <derick9103@gmail.com>
2025-09-30 22:12:58 -0300

    Agrega urls de nuevas vistas crear y guardar

--- direccion/urls.py
+++ direccion/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from direccion import views
 
 direccion_urlpatterns=[
-    path('main_direccion/',views.main_direccion, name='main_direccion')
+    path('main_direccion/',views.main_direccion, name='main_direccion'),
+    path('crear_direccion/',views.crear_direccion, name='crear_direccion'),
+    path('guardar_direccion/',views.guardar_direccion, name='guardar_direccion'),
+
 ]
