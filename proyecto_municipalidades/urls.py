5668d44354ce7eb85390d8df4e654283825d763b
Derikoso <derick9103@gmail.com>
2025-10-02 19:23:02 -0300

    Incluye urlpatterns de usuario

--- proyecto_municipalidades/urls.py
+++ proyecto_municipalidades/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path("direccion/",include(direccion_urlpatterns)),
     path("departamento/",include(departamento_urlpatterns)),
     path("territorial/",include(territorial_urlpatterns)),
+    path("usuario/",include(usuario_urlpatterns)),
     path("incidencia/",include(incidencia_urlpatterns)),
     path('accounts/',include('django.contrib.auth.urls')),
     path('accounts/',include('registration.urls')),
