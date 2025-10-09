830fb7f63e9859c360700805f8d504d06a91c993
zzzpiro-e <ian.valenzuela@cloud.uautonoma.cl>
2025-10-09 19:56:28 -0300

    Creacion funcion editar_incidencia
    - se crea editar_incidencia
    
    - se actualiza main_incidencia para lograr listar las incidencias en el main_incidencias.html

--- incidencia/views.py
+++ incidencia/views.py
@@ -1,17 +1,61 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from registration.models import Profile
-@login_required
+from .models import Incidencia
+from departamento.models import Departamento
+from territorial.models import Territorial
+from encuesta.models import Encuesta
 
+@login_required
 def main_incidencia(request):
     try:
-        profile= Profile.objects.filter(user_id=request.user.id).get()
-    except:
-        messages.add_message(request,messages.INFO, 'Error')
+        profile = Profile.objects.get(user_id=request.user.id)
+    except Profile.DoesNotExist:
+        messages.add_message(request, messages.INFO, 'Error de perfil.')
         return redirect('login')
-    if profile.group_id in [1,2,3,4,5]:
-        template_name = 'incidencia/main_incidencia.html'
-        return render(request,template_name)
-    else: 
+    if profile.group_id in [1, 2, 3, 4, 5]:
+        incidencias = Incidencia.objects.filter(state='Activo')
+        context = {
+            'incidencias': incidencias,
+            'profile': profile
+        }
+        return render(request, 'incidencia/main_incidencia.html', context)
+    else:
+        return redirect('logout')
+    
+@login_required
+def editar_incidencia(request, incidencia_id=None):
+    try:
+        profile = Profile.objects.get(user_id=request.user.id)
+    except Profile.DoesNotExist:
+        messages.add_message(request, messages.INFO, 'Error de perfil.')
+        return redirect('login')
+    if profile.group_id == 1:
+        if request.method == 'POST':
+            inc_id = request.POST.get('incidencia_id')
+            incidencia_a_actualizar = get_object_or_404(Incidencia, id=inc_id)
+            incidencia_a_actualizar.departamento_id = request.POST.get('departamento')
+            incidencia_a_actualizar.territorial_id = request.POST.get('territorial')
+            incidencia_a_actualizar.encuesta_id = request.POST.get('encuesta')
+            incidencia_a_actualizar.titulo = request.POST.get('titulo')
+            incidencia_a_actualizar.tipo = request.POST.get('tipo')
+            incidencia_a_actualizar.ubicacion = request.POST.get('ubicacion')
+            incidencia_a_actualizar.save()
+            messages.add_message(request, messages.INFO, 'Incidencia actualizada con éxito.')
+            return redirect('main_incidencia')
+        else:
+            incidencia = get_object_or_404(Incidencia, id=incidencia_id)
+            departamentos = Departamento.objects.all()
+            territoriales = Territorial.objects.all()
+            encuestas = Encuesta.objects.all()
+            
+            context = {
+                'incidencia': incidencia,
+                'departamentos': departamentos,
+                'territoriales': territoriales,
+                'encuestas': encuestas
+            }
+            return render(request, 'incidencia/editar_incidencia.html', context)
+    else:
         return redirect('logout')
