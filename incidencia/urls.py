9f282d49e94b447b04151828420377d5f8462948
zzzpiro-e <ian.valenzuela@cloud.uautonoma.cl>
2025-10-09 19:54:07 -0300

    Se agregan las url editar

--- incidencia/urls.py
+++ incidencia/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from incidencia import views
 
 incidencia_urlpatterns=[
-     path('main_incidencia/',views.main_incidencia, name='main_incidencia')
+     path('main_incidencia/',views.main_incidencia, name='main_incidencia'),
+     path('editar_incidencia/', views.editar_incidencia, name='editar_incidencia_post'),
+     path('editar_incidencia/<int:incidencia_id>/', views.editar_incidencia, name='editar_incidencia'),
 ]
