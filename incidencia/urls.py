15e6f75c10ad97050fbed5502a2110b0ae778f95
zzzpiro-e <ian.valenzuela@cloud.uautonoma.cl>
2025-10-13 02:47:05 -0300

    Se agrega url de incidencias_usuario_departamento
    - Se agrega la nueva url creada de incidencias_usuario_departamento en la urlpatterns

--- incidencia/urls.py
+++ incidencia/urls.py
@@ -5,4 +5,5 @@ incidencia_urlpatterns=[
      path('main_incidencia/',views.main_incidencia, name='main_incidencia'),
      path('editar_incidencia/', views.editar_incidencia, name='editar_incidencia_post'),
      path('editar_incidencia/<int:incidencia_id>/', views.editar_incidencia, name='editar_incidencia'),
+     path('incidencias_usuario_departamento/', views.incidencias_usuario_departamento, name='incidencias_usuario_departamento'),
 ]
